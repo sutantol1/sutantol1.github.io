b1937c6730cb169c1a323dd163e4c6fcf370769d
monstera8 <sutantol1@my.smccd.edu>
2024-04-29 19:55:24 -0700

    update service-worker.js

--- CIS128/homework4/service-worker.js
+++ CIS128/homework4/service-worker.js
@@ -1,16 +1,16 @@
 var CACHE_VERSION = 'myapp-v1';
 var CACHE_FILES = [
-    'images/offline.png',
-    'images/icon-72.png',
-    'images/icon-128.png',
-    'images/icon-144.png',
-    'images/icon-172.png',
-    'images/icon-196.png',
-    'images/icon-512.png',
-    'images/lightblue.jpg',
-    'images/lightgold.png',
-    'images/narrowScreenshot.png',
-    'images/wideScreenshot.png',
+    '/images/offline.png',
+    '/images/icon-72.png',
+    '/images/icon-128.png',
+    '/images/icon-144.png',
+    '/images/icon-172.png',
+    '/images/icon-196.png',
+    '/images/icon-512.png',
+    '/images/lightblue.jpg',
+    '/images/lightgold.png',
+    '/images/narrowScreenshot.png',
+    '/images/wideScreenshot.png',
     'app.js',
     'index.html',
     'styles.css'
@@ -29,6 +29,31 @@ self.addEventListener('install', event => {
     );
 });
 */
+//code
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(async (cache) => {
+      let ok;
+  
+      console.log('ServiceWorker: Caching files:', c.length, c);
+      try {
+        ok = await cache.addAll(CACHE_FILES);
+      } catch (err) {
+        console.error('sw: cache.addAll');
+        for (let i of CACHE_FILES) {
+          try {
+            ok = await cache.add(i);
+          } catch (err) {
+            console.warn('sw: cache.add',i);
+          }
+        }
+      }
+  
+      return ok;
+    }));
+  
+    console.log('ServiceWorker installed');
+  });
+/*
 self.addEventListener("install", (event) => {
     console.log("Service Worker : Installed!")
     event.waitUntil(
@@ -43,6 +68,7 @@ self.addEventListener("install", (event) => {
         })()
     )
 } )
+*/
 
 self.addEventListener('activate', event => {
     console.log('SW activated');
